399efb072cfff910113fb6804037063eae8d9d96
mathiasbredholt <mathiasbredholt@gmail.com>
2022-04-27 14:28:45 +0200

    white

--- shaders/geomFragmentShader.glsl
+++ shaders/geomFragmentShader.glsl
@@ -4,5 +4,5 @@ precision mediump float;
 
 in vec3 fColor;
 out vec4 FragColor;
-void main() { FragColor = vec4(fColor, 1.0); }
-//void main() { FragColor = vec4(1.0, 1.0, 1.0, 1.0); }
+//void main() { FragColor = vec4(fColor, 1.0); }
+void main() { FragColor = vec4(1.0, 1.0, 1.0, 1.0); }
